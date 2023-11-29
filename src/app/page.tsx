48c0ce52160dbf7850b835ac0acc8f74258b1746
Paddy <Paddygnewton@hotmail.co.uk>
2023-11-29 12:20:02 +0000

    Style modifications

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { Container } from '@mantine/core'
 import PageHeader from './_components/PageHeader'
 
 export default function Home() {
   return (
-    <PageHeader heading={'Dashboard'} subHeading={'Welcome to the mark management system'}/>
+    <>
+    <PageHeader heading={'Dashboard'} subHeading={'Welcome back *User*'}/>
+    <div style={{fontSize:'1.5rem', marginRight:'10rem'}}>
+      <h2>Welcome to your new Mark Management System!</h2>
+      <div>
+        Here you will be able to view a plethora of student information, upload class and assignment marks, gain insights into student performance 
+        through the basic and also the enhanced analytics view to better make student outcome decisions. You can view student circumstances
+        around assignments, exams and other marks which will decide their final outcome from both the exam board and lecturers alike.
+      </div>
+    </div> 
+    </>
   )
 }
--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -12,9 +12,9 @@ export default function Page() {
   return (
     <>
     <PageHeader heading={'Upload'} subHeading={'Upload student marks'}/>
-    <Container size={700}>
+    <Container size={800}> 
     <Select data={classes} label='Please select a class from your taught classes' onSelect={() => {setUploadStatus(true)}}/>
-    <Container mt={25} ta="center" style={{border: '2px dashed black', borderRadius: '5px'}} p={20}>
+    <Container mt={25} ta="center" style={{border: '2px dashed black', borderRadius: '5px'}} p={80} fz={30}>
       <Dropzone
         onDrop={() => {}}
         accept={[MIME_TYPES.csv]}
@@ -29,7 +29,7 @@ export default function Page() {
             <Dropzone.Idle>Upload CSV</Dropzone.Idle>
           </p>
           <p style={{color:"red"}}>
-            Please ensure CSV is in the same format provided from MyPlace or it will be rejected!
+            Please ensure CSV is in the same format provided or it will be rejected!
           </p>
         </div>
       </Dropzone>
