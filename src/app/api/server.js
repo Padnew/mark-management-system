ccfadfe8d0f56910fdb04f5f4f4c50f726a8b745
Paddy <Paddygnewton@hotmail.co.uk>
2024-01-07 16:58:30 +0000

    Added MySQL connection

---
+++ src/app/api/server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+
+const connection = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "voirdire",
+  database: "mms",
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const port = 20502;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get("/getlecturer", async (req, res) => {
+  connection.query(
+    "SELECT * FROM users WHERE role = 1",
+    (err, results, fields) => {
+      if (err) {
+        return res.status(500).json({ error: err });
+      }
+      return res.json(results);
+    }
+  );
+});
+
+app.get("/students", async (req, res) => {
+  connection.query("SELECT * from students", (err, results, fields) => {
+    if (err) {
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+    return res.json(results);
+  });
+});
+
+app.get("/results", async (req, res) => {
+  connection.query("SELECT * from results", (err, results, fields) => {
+    if (err) {
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+    return res.json(results);
+  });
+});
+
+module.exports = app;
