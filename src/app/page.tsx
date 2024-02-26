c733ef7feaa81bb15d96a9f4c781d146e079ce1b
Paddy <Paddygnewton@hotmail.co.uk>
2024-02-26 11:22:46 +0000

    Removed comment block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,24 +18,6 @@ export default function Home() {
         heading={"Dashboard"}
         subHeading={user != null && `Welcome back, ${user?.first_name}!`}
       />
-      {/* <div
-        style={{
-          fontSize: "1.5rem",
-          marginRight: "10rem",
-          paddingLeft: "1rem",
-        }}
-      >
-        <h2>Welcome to your new Mark Management System!</h2>
-        <div>
-          Here you will be able to view a plethora of student information,
-          upload class and assignment marks, gain insights into student
-          performance through the basic and also the enhanced analytics view to
-          better make student outcome decisions. You can view student
-          circumstances around assignments, exams and other marks which will
-          decide their final outcome from both the exam board and lecturers
-          alike.
-        </div>
-      </div> */}
       <Blockquote
         ml="xl"
         mr="xl"
